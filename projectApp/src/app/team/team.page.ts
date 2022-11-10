cde746210779ff8dd6d8a47824f904e70fb8810c
a329782 <a329782@uach.mx>
2022-11-10 16:47:54 -0600

    Se agrega la alerta para unirse a equipo

--- projectApp/src/app/team/team.page.ts
+++ projectApp/src/app/team/team.page.ts
@@ -1,4 +1,6 @@
+import { alertController } from '@ionic/core';
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-team',
@@ -7,9 +9,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    public alertController: AlertController
+  ) { }
 
   ngOnInit() {
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: "Unirse a equipo",
+      cssClass: 'join-team-alert',
+      message: "Ingresa el código del equipo",
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: data => {
+            console.log('Acción cancelada.');
+          }
+        },
+        {
+          text: 'Confirmar',
+          handler: data => {
+            this.sentConfirmationAlert();
+          }
+        }
+      ],
+      inputs: [
+        {
+          placeholder: 'Código',
+          attributes: {
+            maxLength: 6,
+          },
+        },
+      ],
+    });
+    await alert.present()
+  }
+
+  async sentConfirmationAlert() {
+    const alert = await this.alertController.create({
+      message: "Solicitud enviada.",
+      buttons: ["Aceptar"]
+    });
+    await alert.present();
   }
 
 }
