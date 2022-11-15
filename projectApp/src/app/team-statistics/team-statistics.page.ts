51644312919273d5eefb080a71dae08b6923247f
a329782 <a329782@uach.mx>
2022-11-14 19:36:50 -0600

    Se agrega info del jugador dinamica

--- projectApp/src/app/team-statistics/team-statistics.page.ts
+++ projectApp/src/app/team-statistics/team-statistics.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController, NavController } from '@ionic/angular';
+import { modalController } from '@ionic/core';
 
 @Component({
   selector: 'app-team-statistics',
@@ -7,17 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamStatisticsPage implements OnInit {
 
+
+  teams = [
+    {teamId: 0, name: 'Los temerarios'},
+
+  ]
+
   playersWednesday = [
+
     { id:0, name: 'Juan Pérez', numGoals: 14, yellowCards: 5, redCards: 2, gamesPlayed: 10},
     { id:1, name: 'Daniel García', numGoals: 10, yellowCards: 5, redCards: 2, gamesPlayed: 10 },
     { id:2, name: 'Alexander Martínez', numGoals: 8, yellowCards: 5, redCards: 2, gamesPlayed: 10 },
     { id:3, name: 'Mateo López', numGoals: 7, yellowCards: 5, redCards: 2, gamesPlayed: 10 },
     { id:4, name: 'Leonardo Rodríguez', numGoals: 5, yellowCards: 5, redCards: 2, gamesPlayed: 10 }
-  ]
+  ];
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
