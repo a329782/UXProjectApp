a70d6aa3f2d11aa669fdd036ddd4cbb8936d479f
a329782 <a329782@uach.mx>
2022-11-27 19:23:57 -0600

    Small ux and ui fixes

--- projectApp/capacitor.config.ts
+++ projectApp/capacitor.config.ts
@@ -1,10 +1,16 @@
 import { CapacitorConfig } from '@capacitor/cli';
+import { KeyboardResize } from '@capacitor/keyboard';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'projectApp',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    Keyboard: {
+      resize: KeyboardResize.None,
+    },
+  },
 };
 
 export default config;
